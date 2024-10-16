493552a8dcdfbc61513835b61c440f5b0b2b8948
Rynare <rynare.of.legend@gmail.com>
2024-10-16 09:42:19 +0700

    memperbaiki nabarv

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger")
     const navOffcanvas = document.querySelector(".nav-offcanvas")
     const offcanvasCloseBtn = navOffcanvas.querySelector(".close-btn")
+
     hamburger.addEventListener("click", () => {
         navOffcanvas.classList.add("active")
+        document.body.style.overflow = "hidden"
     })
     offcanvasCloseBtn.addEventListener("click", () => {
         navOffcanvas.classList.remove("active")
+        document.body.style.overflow = "auto"
     })
 
     window.addEventListener("hashchange", () => {
-        navOffcanvas.classList.remove("active")
+        if (navOffcanvas.classList.contains("active")) {
+            document.body.style.overflow = "auto"
+            navOffcanvas.classList.remove("active")
+        }
         setActiveNav()
     })
 
@@ -23,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
         target.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
     })
 })
+
 function setActiveNav() {
     const activeNavs = document.querySelectorAll(".nav-offcanvas .active, nav .active")
     const hash = window.location.hash || "#home"
